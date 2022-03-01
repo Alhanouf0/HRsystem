ce1ee74adf66ba7d90782a261936990b43118c33
Alhanouf0 <aalmansour@mrsool.co>
2022-03-01 17:59:23 +0300

    translations commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,25 @@
 Rails.application.routes.draw do
   #get 'users/index'
-  devise_for :users, :path_prefix => 'd'
-  resources :users, :only =>[:show]
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+  # your routes here...
 
-  get 'home/index'
-  resources :divisions
-  resources :targets
-  resources :teams
-  resources :employees
+      devise_for :users, :path_prefix => 'd'
+      resources :users, :only =>[:show]
 
-  match '/users',   to: 'users#index',   via: 'get'
-  match '/users/:id',     to: 'users#show',       via: 'get'
+      get 'home/index'
+      resources :divisions
+      resources :targets
+      resources :teams
+      resources :employees
+
+      match '/users',   to: 'users#index',   via: 'get'
+      match '/users/:id',     to: 'users#show',       via: 'get'
 
 
-  root :to => "home#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+      root :to => "home#index"
+      # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+      # Defines the root path route ("/")
+      # root "articles#index"
+end
 end
